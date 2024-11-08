4a9b36204c71f75eae9b85c156f1eca3e21e92e9
Martin Hernando Luz <martinhernandoluz@gmail.com>
2024-11-08 17:52:00 -0300

    ruta para crear usuario funcionando

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,9 +1,9 @@
-import {create} from '../model/userModel.js'
+import {createUserDB} from '../model/userModel.js'
 
 export const createUser = async (req, res) => {
     const data = req.body;
     try {
-        const result = await create(data);
+        const result = await createUserDB(data);
         res.status(201).json(result);
     } catch (error) {
         res.status(500).json(error.message);
--- src/model/userModel.js
+++ src/model/userModel.js
@@ -1,23 +1,21 @@
 import db from "../config/db.js";
 import bcrypt from "bcryptjs";
 
-const create = async (data) => {
-    try {
-      const { email, password } = data;
+export const createUserDB = async (data) => {
+  try {
+    const { email, password } = data;
 
-      const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hashSync(password, 10);
 
 
-      const sentence = "INSERT INTO usuario (email, password) VALUES ($1, $2)";
-      const { rows } = await db.query(sentence, [email, hashedPassword]);
-  
-      return {
-        message: `Avion ${nombre} insertado con exito`,
-        detail: rows[0],
-      };
-    } catch (error) {
-      throw new Error(error.message);
-    }
-  };
+    const sentence = "INSERT INTO usuario (email, password) VALUES (?, ?)";
+    const [rows] = await db.query(sentence, [email, hashedPassword]);
 
-export { create };
+    return {
+      message: `Usuario ${email} insertado con éxito`,
+      detail: rows,
+    };
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
